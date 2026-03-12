96576fd20447ec9df04dcc63f5afd1cacacc17d4
jmiguelcheq <jmiguel@cheqsystems.com>
2026-03-13 05:19:38 +0800

    Refactor AndroidDriverFactory to use system properties
    Updated driver loading logic to use system properties for configuration, improving flexibility and allowing for environment-specific settings.

--- src/test/java/com/cheq/demo_webshop/factory/AndroidDriverFactory.java
+++ src/test/java/com/cheq/demo_webshop/factory/AndroidDriverFactory.java
@@ -14,47 +14,58 @@ import com.cheq.demo_webshop.utils.ConfigReader;
  * for Chrome browser automation using Appium and UiAutomator2.
  */
 public class AndroidDriverFactory {
+
     /**
      * Initializes the AndroidDriver instance for the current thread.
      *
      * Sets up the driver with UiAutomator2 options for Chrome browser automation
      * on an Android device.
+     *
      * @throws Exception
      */
     public static AndroidDriver loadDriver() throws Exception {
 
-    	String env = System.getProperty("env", "dev");
+        String env = System.getProperty("env", "dev");
         ConfigReader.loadProperties(env);
 
-        String platformName = ConfigReader.get("platformName");
-        String deviceName = ConfigReader.get("deviceName");
-        String platformVersion = ConfigReader.get("platformVersion");
-        String automationName = ConfigReader.get("automationName");
-        String browserName = ConfigReader.get("browserName");
-        String chromeDriverFilePath = ConfigReader.get("chromedriverPath");
-        String urlPath = ConfigReader.get("urlPath");
-        
+        String platformName = System.getProperty("platformName", ConfigReader.get("platformName"));
+        String deviceName = System.getProperty("deviceName", ConfigReader.get("deviceName"));
+        String platformVersion = System.getProperty("platformVersion", ConfigReader.get("platformVersion"));
+        String automationName = System.getProperty("automationName", ConfigReader.get("automationName"));
+        String browserName = System.getProperty("browserName", ConfigReader.get("browserName"));
+        String chromeDriverFilePath = System.getProperty("chromedriverPath", ConfigReader.get("chromedriverPath"));
+        String urlPath = System.getProperty("appium.server.url", ConfigReader.get("urlPath"));
+
         UiAutomator2Options options = new UiAutomator2Options();
-        
+
         try {
             options.setPlatformName(platformName);
             options.setDeviceName(deviceName);
-            options.setPlatformVersion(platformVersion);
+
+            if (platformVersion != null && !platformVersion.isBlank()) {
+                options.setPlatformVersion(platformVersion);
+            }
+
             options.setAutomationName(automationName);
             options.setCapability("browserName", browserName);
+
             String chromeDriverPath = new File(chromeDriverFilePath).getAbsolutePath();
             options.setCapability("chromedriverExecutable", chromeDriverPath);
 
             URL url = URI.create(urlPath).toURL();
             AndroidDriver driver = new AndroidDriver(url, options);
-            
+
             System.out.println("Appium Driver initialized successfully");
-            
+            System.out.println("platformName = " + platformName);
+            System.out.println("deviceName = " + deviceName);
+            System.out.println("platformVersion = " + platformVersion);
+            System.out.println("browserName = " + browserName);
+            System.out.println("appiumServerUrl = " + urlPath);
+
             return driver;
+
         } catch (Exception e) {
-            throw new Exception("Error initializing the Appium driver: " + e.getMessage());
+            throw new Exception("Error initializing the Appium driver: " + e.getMessage(), e);
         }
-
     }
-
 }
